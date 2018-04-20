78e12f7aaad52cbeb274974879d03be9908f9ec7
Mitt Coats <mittcoats@gmail.com>
2018-04-19 20:27:08 -0700

    clean up routes, navbar, and naming

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { ProductsComponent } from './products/products.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { OrderSuccessComponent } from './order-success/order-success.component';
-import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { OrdersComponent } from './orders/orders.component';
 import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 import { LoginComponent } from './login/login.component';
@@ -29,7 +29,7 @@ import { LoginComponent } from './login/login.component';
     ShoppingCartComponent,
     CheckoutComponent,
     OrderSuccessComponent,
-    MyOrdersComponent,
+    OrdersComponent,
     AdminProductsComponent,
     AdminOrdersComponent,
     LoginComponent
@@ -46,6 +46,7 @@ import { LoginComponent } from './login/login.component';
       { path: 'shopping-cart', component: ShoppingCartComponent },
       { path: 'checkout', component: CheckoutComponent },
       { path: 'order-success', component: OrderSuccessComponent },
+      { path: 'my/orders', component: OrdersComponent },
       { path: 'login', component: LoginComponent },
       { path: 'admin/products', component: AdminProductsComponent },
       { path: 'admin/orders', component: AdminOrdersComponent }
