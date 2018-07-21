df7e18aebe39bb9f26f559250471e51b80e9a4c5
Mitt Coats <mittcoats@gmail.com>
2018-07-21 17:14:32 -0400

    add search function to admin products page

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,21 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from '../../product.service';
+import { Subscription } from 'rxjs/Subscription';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-  products$;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products: Product[];
+  filteredProducts: Product[];
+  subscription: Subscription;
 
   constructor(
     private productService: ProductService
   ) {
-    this.products$ = this.productService.getAll();
+    this.productService.getAll().subscribe(products =>
+      this.filteredProducts = this.products = products);
   }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  filter(query: string) {
+    console.log(query);
+
+    this.filteredProducts = (query) ?
+      this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
+      this.filteredProducts = this.products;
   }
 
 }
