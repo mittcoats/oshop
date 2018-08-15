e08bdf352a5dfb6f9138f1c359727dcb201b614c
Mitt Coats <mittcoats@gmail.com>
2018-08-15 08:26:57 -0400

    wire up product categories; add switchmap to productsService call in products component

--- src/app/category.service.ts
+++ src/app/category.service.ts
@@ -8,7 +8,7 @@ export class CategoryService {
     private db: AngularFireDatabase
   ) { }
 
-  getCategories() {
+  getAll() {
     return this.db.list('/categories', {
       query: {
         orderByChild: 'name'
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { CategoryService } from '../category.service';
+import { ActivatedRoute } from '../../../node_modules/@angular/router';
+import { Product } from '../models/product';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-products',
@@ -6,8 +11,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+  products: any;
+  //products: Product[] = [];
+  filteredProducts: Product[] = [];
+  products$;
+  categories$;
+  category: string;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService,
+    private categoryService: CategoryService,
+  ) {
+
+    productService
+      .getAll()
+      .switchMap(products => {
+        this.products = products;
+        return route.queryParamMap
+      })
+      .subscribe(params => {
+        this.category = params.get('category');
+
+        this.filteredProducts = (this.category) ?
+          this.products.filter(p => p.category === this.category) :
+          this.products;
+      });
+    
+    this.categories$ = categoryService.getAll();
+  }
 
   ngOnInit() {
   }
