03e904e1a983c5b6a981fc4b8701b80987953377
Mitt Coats <mittcoats@gmail.com>
2018-08-15 08:45:56 -0400

    remove 'app-' from product-filter selector

--- src/app/products/product-filter/product-filter.component.ts
+++ src/app/products/product-filter/product-filter.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { CategoryService } from '../../category.service';
 
 @Component({
-  selector: 'app-product-filter',
+  selector: 'product-filter',
   templateUrl: './product-filter.component.html',
   styleUrls: ['./product-filter.component.css']
 })
